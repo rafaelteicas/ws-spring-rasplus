f99678d0d159e1cb8a0ff8200acde2a5ca289882
Rafael <rafaelteicas@gmail.com>
2024-03-07 15:09:55 -0300

    create exception and handlers

---
+++ src/main/java/com/client/ws/rasmooplus/exception/handler/ResourceHandler.java
@@ -0,0 +1,68 @@
+package com.client.ws.rasmooplus.exception.handler;
+
+import com.client.ws.rasmooplus.dto.error.ErrorResponseDto;
+import com.client.ws.rasmooplus.exception.BadRequestException;
+import com.client.ws.rasmooplus.exception.NotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class ResourceHandler {
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ErrorResponseDto> notFoundException(NotFoundException n) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(ErrorResponseDto
+                        .builder()
+                        .message(n.getMessage())
+                        .httpStatus(HttpStatus.NOT_FOUND)
+                        .statusCode(HttpStatus.NOT_FOUND
+                        .value())
+                        .build());
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ErrorResponseDto> badRequestException(BadRequestException b) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(ErrorResponseDto
+                        .builder()
+                        .message(b.getMessage())
+                        .httpStatus(HttpStatus.NOT_FOUND)
+                        .statusCode(HttpStatus.NOT_FOUND.value())
+                        .build());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponseDto> badRequestException(MethodArgumentNotValidException m) {
+        Map<String, String> messages = new HashMap<>();
+        m.getBindingResult().getAllErrors().forEach(error -> {
+            String field = ((FieldError) error).getField();
+            String message = error.getDefaultMessage();
+            messages.put(field, message);
+        });
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorResponseDto
+                .builder()
+                .message(Arrays.toString(messages.entrySet().toArray()))
+                .httpStatus(HttpStatus.BAD_REQUEST)
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .build());
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<ErrorResponseDto> badRequestException(DataIntegrityViolationException d) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorResponseDto
+                .builder()
+                .message(d.getMessage())
+                .httpStatus(HttpStatus.BAD_REQUEST)
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .build());
+    }
+}
